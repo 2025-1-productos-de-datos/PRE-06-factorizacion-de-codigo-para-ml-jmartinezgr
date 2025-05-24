123030a5cae9726105a65d6e58f3c93a76ef610e
Juan José Martinez Grisales <josemargri3@gmail.com>
2025-05-24 08:35:57 -0500

    fix: fix bad name of module main.py should be __main__.py

--- homework/src/_internals/save_model_if_better.py
+++ homework/src/_internals/save_model_if_better.py
@@ -10,7 +10,12 @@ def save_model_if_better(model, x_test, y_test, save_path="models/estimator.pkl"
     """Save the model if it performs better than the existing one."""
     best_model = None
     if os.path.exists(save_path):
-        with open(save_path, "rb") as file:
-            best_model = pickle.load(file)
+        try:
+            with open(save_path, "rb") as file:
+                best_model = pickle.load(file)
+        except (EOFError, pickle.UnpicklingError):
+            print("⚠️ El archivo del modelo existente está vacío o dañado. Se ignorará.")
+            best_model = None
+
     best_model = compare_models(model, best_model, x_test, y_test)
     save_model(best_model, save_path)
